068588150702e036d542d4744c8a1223a76da286
giusseppemiranda <70769265+giusseppemiranda@users.noreply.github.com>
2021-09-23 11:52:25 -0500

    Add files via upload

---
+++ lab0320151864/src/lab0320151864/Lab0320151864.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab0320151864;
+
+/**
+ *
+ * @author giovanni.miranda
+ */
+public class Lab0320151864 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ lab0320151864/src/pe/edu/pucp/musicalstore/config/DBManager.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.musicalstore.config;
+
+/**
+ *
+ * @author giovanni.miranda
+ */
+public class DBManager {
+    public static String url ="jdbc:mysql://"+"claselp220212.csed1r5vqsyp.us-east-1.rds.amazonaws.com"+":3303"+"/inf28220212";
+    public static String user = "root";
+    public static String password = "LP2INF20212";
+}
